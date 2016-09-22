9ba3718ad65bbf55cb3070d2a1cfcaa4d7db6567
Henrik Treadup <henrik.treadup@gmail.com>
2016-09-22 21:57:51 +0200

    Install use-package. Use use-package to install other packages.

--- init.el
+++ init.el
@@ -1,3 +1,40 @@
+;;
+;; Package management
+;;
 
-;Hide startup screen
+(require 'package)
+
+;; We do not want to call package-initialize again after the init file has
+;; been loaded.
+(setq package-enable-at-startup nil)
+
+;; Add package archives
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(defvar my-packages '(better-defaults
+                      projectile
+                      clojure-mode
+                      cider))
+
+;; Bootstrap 'use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;
+;; Load packages
+;;
+
+;; The :ensure t tells use-package to load the package from the package-archives
+;; if the package is not already installed.
+
+(use-package better-defaults
+  :ensure t)
+
+(use-package magit
+  :ensure t)
+
+;; Hide startup screen
 (setq inhibit-startup-screen t)
