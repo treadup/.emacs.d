e424476fd2f7a319d2576fe0b30f8b1951bb7f3c
Henrik Treadup <henrik.treadup@gmail.com>
2016-09-23 19:34:23 +0200

    Configuration for cider

--- elisp/for-clojure.el
+++ elisp/for-clojure.el
@@ -35,3 +35,31 @@
   :ensure t)
 
 
+
+;;
+;; Cider
+;;
+(defun config-for-cider ()
+
+  ;; provides minibuffer documentation for the code you're typing into the repl
+  (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+  ;; go right to the REPL buffer when it's finished connecting
+  (setq cider-repl-pop-to-buffer-on-connect t)
+
+  ;; When there's a cider error, show its buffer and switch to it
+  (setq cider-show-error-buffer t)
+  (setq cider-auto-select-error-buffer t)
+
+  ;; Where to store the cider history.
+  (setq cider-repl-history-file "~/.emacs.d/cider-history")
+
+  ;; Wrap when navigating history.
+  (setq cider-repl-wrap-history t))
+
+
+(use-package cider
+  :ensure t
+  :config
+  (config-for-cider))
+
