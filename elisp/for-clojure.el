101cbd95ec39c6a3519f7e0750e7efad833731b7
Henrik Treadup <henrik.treadup@gmail.com>
2016-09-23 19:40:40 +0200

    Keybindings for Cider

--- elisp/for-clojure.el
+++ elisp/for-clojure.el
@@ -34,6 +34,35 @@
 (use-package clojure-mode-extra-font-locking
   :ensure t)
 
+;;
+;; Cider Keybindings
+;;
+
+(defun cider-start-http-server ()
+  (interactive)
+  (cider-load-current-buffer)
+  (let ((ns (cider-current-ns)))
+    (cider-repl-set-ns ns)
+    (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
+    (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
+
+(defun cider-refresh ()
+   (interactive)
+   (cider-interactive-eval (format "(user/reset)")))
+
+(defun cider-user-ns ()
+   (interactive)
+   (cider-repl-set-ns "user"))
+
+
+(defun extra-keybindings-for-cider ()
+    (define-key clojure-mode-map (kbd "C-c C-v") 'cider-start-http-server)
+    (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
+    (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
+    (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns))
+
+;; integration with a Clojure REPL
+;; https://github.com/clojure-emacs/cider
 
 
 ;;
@@ -55,11 +84,15 @@
   (setq cider-repl-history-file "~/.emacs.d/cider-history")
 
   ;; Wrap when navigating history.
-  (setq cider-repl-wrap-history t))
+  (setq cider-repl-wrap-history t)
 
+  (extra-keybindings-for-cider))
+
+;;
+;; Cider package
+;;
 
 (use-package cider
   :ensure t
   :config
   (config-for-cider))
-
