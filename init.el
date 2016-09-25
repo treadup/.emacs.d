382f1c512f2ea4d4213e531970706ad1e4bf33e5
Henrik Treadup <henrik.treadup@gmail.com>
2016-09-25 16:47:31 +0200

    Changed to bas16 themes.

--- init.el
+++ init.el
@@ -40,7 +40,10 @@
 ;;  :ensure t)
 ;;
 ;; This has the problem that the current line highlighting disappears.
-(load-theme 'deeper-blue)
+
+;; Lets try some of the built in themes.
+;; I can't really find one that I like.
+;; (load-theme 'deeper-blue)
 
 ;; Here are some other good built in themes.
 ;; deeper-blue
@@ -53,6 +56,18 @@
 ;; is the Tomorrow Night Bright.
 ;; Unfortunately it does not seem to be available via any package repos.
 ;; https://github.com/chriskempson/tomorrow-theme/blob/master/GNU%20Emacs/color-theme-tomorrow.el
+
+(use-package base16-theme
+  :ensure t
+  :config
+  (load-theme 'base16-solar-flare t)) ;; The t at the end load the them without giving any security warnings.
+
+;; Some other good choices for themes
+;; base16-materia
+;; base16-oceanicnext
+;; base16-harmonic16-dark
+;; base16-solar-flare
+;; base16-solarized-dark
 
 ;; The smex package provides an Ido like interface for M-x.
 (use-package smex
