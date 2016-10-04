63d3d37686aec55505c4128d75e6106f81c9ac72
Henrik Treadup <henrik.treadup@gmail.com>
2016-10-04 22:01:19 +0200

    Changed tab handling to follow the rules at work.

--- elisp/for-php.el
+++ elisp/for-php.el
@@ -7,15 +7,29 @@
 ;;
 ;; PHP
 ;;
-
+(defun personal-php-indent ()
+    (tabify (point-min) (point-max)))
+  
 (defun personal-php-mode-config ()
     (message "Applying personalized PHP configuration")
+
+    ;; We use make-local-variable below so that the settings
+    ;; only apply to the local buffer.
+    
+    ;; Indent using tab characters.
     (make-local-variable 'indent-tabs-mode)
+    (setq indent-tabs-mode t)
+    
+    ;; Use two character indent when indenting PHP code.
     (make-local-variable 'c-basic-offset)
-    (make-local-variable 'tab-width)
     (setq c-basic-offset 2)
+
+    ;; The width of the tab should be 2 characters.
+    (make-local-variable 'tab-width)
     (setq tab-width 2)
-    (setq indent-tabs-mode t))
+    
+    ;; Add hook to indent PHP code before saving.
+    (add-hook 'before-save-hook 'personal-php-indent nil t))
 
 (use-package php-mode
   :ensure t
