0028ebd58d3ec4dec8a9410498de75b520e7648e
Henrik Treadup <henrik.treadup@gmail.com>
2016-10-04 20:19:33 +0200

    Moved user interface specific things to separate for-ui.el file.

--- elisp/for-ui.el
+++ elisp/for-ui.el
@@ -1,7 +1,35 @@
+;;;
+;;; Customizations for User Interface
+;;;
+;;; This file contains user interface specific things.
+;;;
+
 ;;
-;; Customizations for UI
+;; User Interface
 ;;
+
+;; Hide startup screen
+(setq inhibit-startup-screen t)
+
+;; Show line numbers
+(global-linum-mode)
+
+;; Show menu bar
+(menu-bar-mode t)
+
+;; Increase font size.
+;; Play with the :height attribute to change size
+(set-face-attribute 'default nil :height 120)
+
+;; No cursor blinking
+(blink-cursor-mode 0)
+
+;; Title bar path
+;; Full path in the title bar
+(setq-default frame-title-format "%b (%f)")
+
+;; Set the width and height of the emacs window in characters.
+(setq initial-frame-alist '((top . 0) (left . 0) (width . 130) (height . 40)))
 
 ;; Turn of the visual bell.
-
 
--- init.el
+++ init.el
@@ -90,33 +90,6 @@
   :config
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
-;;;
-;;; User Interface
-;;;
-
-;; Hide startup screen
-(setq inhibit-startup-screen t)
-
-;; Show line numbers
-(global-linum-mode)
-
-;; Show menu bar
-(menu-bar-mode t)
-
-;; Increase font size.
-;; Play with the :height attribute to change size
-(set-face-attribute 'default nil :height 120)
-
-;; No cursor blinking
-(blink-cursor-mode 0)
-
-;; Title bar path
-;; Full path in the title bar
-(setq-default frame-title-format "%b (%f)")
-
-;; Set the width and height of the emacs window in characters.
-(setq initial-frame-alist '((top . 0) (left . 0) (width . 130) (height . 40)))
-
 ;;
 ;; Clipboard interaction
 ;;
@@ -250,6 +223,11 @@
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
 ;;
+;; User interface specific setup
+;;
+(load "for-ui.el")
+
+;;
 ;; Clojure specific setup
 ;;
 (load "for-clojure.el")
@@ -258,3 +236,5 @@
 ;; Shell specific setup
 ;;
 (load "for-shell.el")
+
+
