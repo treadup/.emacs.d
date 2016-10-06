088b08900d0bceedcea31304dbb3578b1aee8ec5
Henrik Treadup <henrik.treadup@gmail.com>
2016-10-07 01:19:06 +0200

    Improved launching of shells

--- elisp/for-shell.el
+++ elisp/for-shell.el
@@ -1,4 +1,16 @@
 ;; Create keybindings for the different terminal alternatives.
-(global-set-key (kbd "C-c s") 'shell)
-(global-set-key (kbd "C-c t") 'term)
-(global-set-key (kbd "C-c e") 'eshell)
+
+(defun shell-below ()
+  (progn (split-window-below) (other-window 1) (shell)))
+
+(defun term-below ()
+  (progn (split-window-below) (other-window 1) (term "/bin/bash")))
+
+(defun eshell-below ()
+  (progn (split-window-below) (other-window 1) (eshell)))
+
+(global-set-key (kbd "C-c s") (lambda () (interactive) (shell-below)))
+(global-set-key (kbd "C-c t") (lambda () (interactive) (term-below)))
+(global-set-key (kbd "C-c e") (lambda () (interactive) (eshell-below)))
+
+
