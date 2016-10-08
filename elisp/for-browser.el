fe15f3b9f6c0e3f11199ae42353ca8226372fcd6
Henrik Treadup <henrik.treadup@booli.se>
2016-10-08 14:14:26 +0200

    More browser stuff

--- elisp/for-browser.el
+++ elisp/for-browser.el
@@ -7,5 +7,16 @@
 
 ;; It seems like you have to enter http:// infront of the url. Otherwise the
 ;; browse-url command will not do anything.
+;; (browse-url) will prompt you for the URL interactively.
+;; (browse-url "http://the.url.goes.here/some/path") will navigate to the url directly. 
+
+;; Using the browse-url function I should be able to do something that is similar to
+;; the Mac program Alfred.
+;;
+;; (defun goog)
+;; (defun google)
+;; (defun amz)
+;; (defun amazon)
+;; (defun red)
 
 ;; (setq browse-url-browser-function 'browse-url-firefox)
