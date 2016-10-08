1f77f7f5b10c6ce09a8b3c28fa44ae29fdfa02a4
Henrik Treadup <henrik.treadup@gmail.com>
2016-10-08 10:50:35 +0200

    Keybinding for ielm shell.

--- elisp/for-shell.el
+++ elisp/for-shell.el
@@ -9,8 +9,12 @@
 (defun eshell-below ()
   (progn (split-window-below) (other-window 1) (eshell)))
 
+(defun ielm-below ()
+    (progn (split-window-below) (other-window 1) (ielm)))
+
 (global-set-key (kbd "C-c s") (lambda () (interactive) (shell-below)))
 (global-set-key (kbd "C-c t") (lambda () (interactive) (term-below)))
 (global-set-key (kbd "C-c e") (lambda () (interactive) (eshell-below)))
+(global-set-key (kbd "C-c i") (lambda () (interactive) (ielm-below)))
 
 
