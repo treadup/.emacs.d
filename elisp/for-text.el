6f9d706fe0785d4b9daacab493089e62250b31db
Henrik Treadup <henrik.treadup@gmail.com>
2016-10-09 21:39:46 +0200

    Set tab width to 4.

--- elisp/for-text.el
+++ elisp/for-text.el
@@ -9,7 +9,11 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . text-mode))
 
 ;; In general you can fill a paragraph by pressing the M-q
-;; key combination. This will reflow the text. 
+;; key combination. This will reflow the text.
+
+(defun personal-configure-text-mode ()
+  (setq-default tab-width 4)
+  (turn-on-auto-fill))
 
 ;; Turn on autofill-mode for text modes.
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'text-mode-hook 'personal-configure-text-mode)
