80314940bb03a1b561ea93d72030eeabc2ccd775
Henrik Treadup <henrik.treadup@gmail.com>
2016-10-14 22:13:36 +0200

    Removed dead code from for-python. Added better comment.

--- elisp/for-python.el
+++ elisp/for-python.el
@@ -6,15 +6,13 @@
 ;; Jedi
 ;;
 
+;; I tried using jedi for auto-complete first but I could not get it
+;; to work. Switched to company and company jedi instead.
 
 (use-package company-jedi
   :ensure t)
 
 (defun personal-python-mode-setup ()
   (add-to-list 'company-backends 'company-jedi))
-
-;; (print company-backends)
-
-;; (personal-python-mode-setup)
 
 (add-hook 'python-mode-hook 'personal-python-mode-setup)
