b45bd2ad83157eb0a2a48c2ffea60c1dd1e53060
Henrik Treadup <henrik.treadup@gmail.com>
2016-10-15 02:50:41 +0200

    Added git-gutter

--- elisp/for-magit.el
+++ elisp/for-magit.el
@@ -1,6 +1,10 @@
 ;;;
-;;; Customizations for Magit
+;;; Customizations for Git
 ;;;
+
+(use-package git-gutter
+  :ensure t
+  :config (global-git-gutter-mode 1))
 
 (global-set-key (kbd "C-x g") 'magit-status)
 ;; https://magit.vc/manual/magit/Getting-started.html#Getting-started
