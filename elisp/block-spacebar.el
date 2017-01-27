289d343f18995d22c4a959992d83ffea93c4e257
Henrik Treadup <henrik.treadup@booli.se>
2017-01-27 10:31:01 +0100

    The real block spacebar code.

--- elisp/block-spacebar.el
+++ elisp/block-spacebar.el
@@ -1,13 +1,19 @@
 ;; This is a bit of a weird one.
 ;; At work we need to use tabs to indent the code.
 
+
+;; http://ergoemacs.org/emacs/emacs_tabs_space_indentation_setup.html
+(defun personal-insert-tab-char ()
+  "Insert a tab char. (ASCII 9, \t)"
+  (interactive)
+  (insert "\t"))
+
 (defun is-whitespace-string-p (str)
   (eql (string-match "^[ \t]*$" str) 0))
 
 (defun line-beginning-to-point-string ()
   (let ((start (line-beginning-position))
         (end (point)))
-    ;; (message "start = %d, end = %d" start end)
     (buffer-substring-no-properties start end)))
 
 (defun whitespace-to-point-p ()
@@ -15,13 +21,18 @@
     (is-whitespace-string-p linetopoint)))
 
 (defun conditional-space-insert-command ()
-  (message "Spacebar pressed")
-  (unless (whitespace-to-point-p)
-    (self-insert-command 1)))
+  (if (whitespace-to-point-p)
+      (personal-insert-tab-char)
+      (self-insert-command 1)))
+
+(defun conditional-backspace-command ()
+  (interactive)
+  (if (whitespace-to-point-p)
+      (progn
+        (delete-backward-char)
+        (delete-backward-char))
+      (delete-backward-char)))
 
 ;; The following keybindings are useful for debugging.
 ;; (global-set-key (kbd "C-c l") (lambda () (interactive) (message "%s" (line-beginning-to-point-string))))
 ;; (global-set-key (kbd "C-c w") (lambda () (interactive) (message "%s" (whitespace-to-point-p))))
-
-;; Do not do this as a global set key. Blocking the spacebar should be set on a per mode basis.
-;; (global-set-key (kbd "SPC") (lambda () (interactive) (conditional-space-insert-command)))
