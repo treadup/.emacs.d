3e2c18e7e3c4c6dafe4c913cb47ed18509e23c6f
Henrik Treadup <henrik.treadup@booli.se>
2017-01-27 19:39:34 +0100

    Removed keybindings for starting different shells.

--- elisp/for-shell.el
+++ elisp/for-shell.el
@@ -1,20 +1,25 @@
 ;; Create keybindings for the different terminal alternatives.
 
 (defun shell-below ()
+  (interactive)
   (progn (split-window-below) (other-window 1) (shell)))
 
 (defun term-below ()
+  (interactive)
   (progn (split-window-below) (other-window 1) (term "/bin/bash")))
 
 (defun eshell-below ()
+  (interactive)
   (progn (split-window-below) (other-window 1) (eshell)))
 
 (defun ielm-below ()
-    (progn (split-window-below) (other-window 1) (ielm)))
+  (interactive)
+  (progn (split-window-below) (other-window 1) (ielm)))
 
-(global-set-key (kbd "C-c s") (lambda () (interactive) (shell-below)))
-(global-set-key (kbd "C-c t") (lambda () (interactive) (term-below)))
-(global-set-key (kbd "C-c e") (lambda () (interactive) (eshell-below)))
-(global-set-key (kbd "C-c i") (lambda () (interactive) (ielm-below)))
+;; These keybindings conflict with org mode.
+; (global-set-key (kbd "C-c s") (lambda () (interactive) (shell-below)))
+; (global-set-key (kbd "C-c t") (lambda () (interactive) (term-below)))
+; (global-set-key (kbd "C-c e") (lambda () (interactive) (eshell-below)))
+; (global-set-key (kbd "C-c i") (lambda () (interactive) (ielm-below)))
 
 
