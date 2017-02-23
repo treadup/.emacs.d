5119b34ea617cc4a094c9361c5b72e1610c5bdb8
Henrik Treadup <henrik.treadup@gmail.com>
2017-02-23 23:08:59 +0100

    Added anaconda-mode

--- elisp/for-python.el
+++ elisp/for-python.el
@@ -2,6 +2,13 @@
 ;;; Customizations for Python
 ;;;
 
+(use-package anaconda-mode
+  :ensure t
+  :config (progn
+            (add-hook 'python-mode-hook 'anaconda-mode)
+;            (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+            ))
+
 ;;
 ;; Jedi
 ;;
@@ -9,10 +16,10 @@
 ;; I tried using jedi for auto-complete first but I could not get it
 ;; to work. Switched to company and company jedi instead.
 
-(use-package company-jedi
-  :ensure t)
+;(use-package company-jedi
+;  :ensure t)
 
-(defun personal-python-mode-setup ()
-  (add-to-list 'company-backends 'company-jedi))
+;(defun personal-python-mode-setup ()
+;  (add-to-list 'company-backends 'company-jedi))
 
-(add-hook 'python-mode-hook 'personal-python-mode-setup)
+;(add-hook 'python-mode-hook 'personal-python-mode-setup)
