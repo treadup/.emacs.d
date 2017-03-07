166edcef402f866d623ca9e5cba6ec41dd008c32
Henrik Treadup <henrik.treadup@booli.se>
2017-03-07 19:49:57 +0100

    Sunrise Commander reset panes.

--- elisp/for-sunrise-commander.el
+++ elisp/for-sunrise-commander.el
@@ -5,3 +5,11 @@
 (use-package sunrise-commander
   :ensure t)
 
+
+;; https://www.emacswiki.org/emacs/Sunrise_Commander_Tips
+(defun sr-reset-panes ()
+      "Hard-reset SC panes."
+      (interactive)
+      (when sr-running (sr-setup-windows)))
+
+
