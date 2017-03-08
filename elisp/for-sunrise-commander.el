062630f2b3f488364076dd34cf73fe7eefc81eb3
Henrik Treadup <henrik.treadup@booli.se>
2017-03-08 17:53:04 +0100

    Comment on how Sunrise Commander seems to work.

--- elisp/for-sunrise-commander.el
+++ elisp/for-sunrise-commander.el
@@ -6,10 +6,16 @@
   :ensure t)
 
 
-;; https://www.emacswiki.org/emacs/Sunrise_Commander_Tips
-(defun sr-reset-panes ()
-      "Hard-reset SC panes."
-      (interactive)
-      (when sr-running (sr-setup-windows)))
+;; To reset the panes when Sunrise Commander gets messed up you can
+;; use the sunrise-cd command.
+
+;; Sunrise Commander seems to get messed up when you press the Enter
+;; key when standing over the file selection line.
+;; Instead you should probably press the / key.
+
+;; To open one of the windows to the notes folder on fern change one
+;; of the panes to show the following folder.
+;; /ubuntu@fern.peacefulrainforest.org:/home/ubuntu/notes:
+
 
 
