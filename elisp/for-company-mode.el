6cd495bc36da990a4e28a288be529ff527874f1e
Henrik Treadup <henrik.treadup@gmail.com>
2017-03-16 17:41:11 +0100

    Use company mode and company quickhelp mode globally.

--- elisp/for-company-mode.el
+++ elisp/for-company-mode.el
@@ -1,8 +1,28 @@
 ;;;
-;;; Customization for company mode
+;;; Customizations for company mode
 ;;;
+
+
+;; I tried the auto-complete package and tried to get jedi working
+;; with it however I was unable to. Instead I am going to try company
+;; mode.
+
+;(use-package auto-complete
+;  :ensure t)
 
 (use-package company
   :ensure t)
 
+;; Use company mode in all buffers.
 (add-hook 'after-init-hook 'global-company-mode)
+
+(use-package company-quickhelp
+  :ensure t
+  :config)
+
+;; Turn on the quickhelp in all buffers.
+(company-quickhelp-mode 1)
+
+
+
+
