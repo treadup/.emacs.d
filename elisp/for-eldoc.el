86d204e7c994ceac799292f8dad6a897631abcc0
Henrik Treadup <henrik.treadup@gmail.com>
2017-03-17 17:17:01 +0100

    Enable eldoc globally.

--- elisp/for-eldoc.el
+++ elisp/for-eldoc.el
@@ -4,7 +4,8 @@
 
 ;; The eldoc mode comes built in with Emacs.
 
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+;; (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+;; (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+;; (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(global-eldoc-mode 1)
 
