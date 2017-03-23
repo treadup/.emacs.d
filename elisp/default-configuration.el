c59dd6faf0082836c2460f7a7f5db3b52fc6b4ef
Henrik Treadup <henrik.treadup@gmail.com>
2017-03-23 20:10:13 +0100

    Removed work host specific configuration. Removed work specific configuration.

--- elisp/default-configuration.el
+++
@@ -1,8 +0,0 @@
-;;;
-;;; Default configuration
-;;;
-
-;; The default configuration when the hostname does not
-;; have any special configuration.
-
-(message "Applying default configuration")
--- elisp/hickory-configuration.el
+++
@@ -1,7 +0,0 @@
-;;;
-;;; Hickory configuration
-;;;
-
-;; Configuration that is run on the computer with hostname hickory.
-(message "Applying hickory configuration")
-(load "for-work-configuration.el")
