ce0337d6d4e167e8fc8124ed08ba4c3e06168e26
Henrik Treadup <henrik.treadup@gmail.com>
2017-04-02 08:51:52 +0200

    Cleaned up for-python.el

--- elisp/for-python.el
+++ elisp/for-python.el
@@ -28,18 +28,5 @@
 ;; (eval-after-load "company"
 ;;  '(add-to-list 'company-backends 'company-anaconda))
 
-;;
-;; Jedi
-;;
-
-;; I tried using jedi for auto-complete first but I could not get it
-;; to work. Switched to company and company jedi instead.
-
-;(use-package company-jedi
-;  :ensure t)
-
-;(defun personal-python-mode-setup ()
-;  (add-to-list 'company-backends 'company-jedi))
-
-;(add-hook 'python-mode-hook 'personal-python-mode-setup)
-
+;; I might want to think about using pyenv mode.
+;; https://github.com/proofit404/pyenv-mode
