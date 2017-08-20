7f09aea82b0596783c8aca88955a13b2c41bfbed
Henrik Treadup <henrik.treadup@gmail.com>
2017-08-20 20:14:38 +0200

    Keybind C-c C-o to browse-url-at-point

--- elisp/for-browser.el
+++ elisp/for-browser.el
@@ -8,7 +8,11 @@
 ;; It seems like you have to enter http:// infront of the url. Otherwise the
 ;; browse-url command will not do anything.
 ;; (browse-url) will prompt you for the URL interactively.
-;; (browse-url "http://the.url.goes.here/some/path") will navigate to the url directly. 
+;; (browse-url "http://the.url.goes.here/some/path") will navigate to the url directly.
+(global-set-key (kbd "C-c C-o") (lambda () (interactive) (browse-url-at-point)))
+
+;; (browse-url-at-point) will browse the url at the point.
+;; C-c C-o should be bound to browse-url-at-point. 
 
 ;; Using the browse-url function I should be able to do something that is similar to
 ;; the Mac program Alfred.
@@ -19,4 +23,4 @@
 ;; (defun amazon)
 ;; (defun red)
 
-;; (setq browse-url-browser-function 'browse-url-firefox)
+
