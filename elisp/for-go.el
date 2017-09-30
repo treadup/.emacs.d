e690050c2c44050137d150becde65b8c6358c42d
Henrik Treadup <henrik.treadup@gmail.com>
2017-09-30 07:12:56 +0200

    Set Go formatting.

--- elisp/for-go.el
+++ elisp/for-go.el
@@ -4,3 +4,9 @@
 
 (use-package go-mode
   :ensure t)
+
+(add-hook 'go-mode-hook
+          (lambda ()
+            ;; (add-hook 'before-save-hook 'gofmt-before-save)
+            (setq tab-width 4)
+            (setq indent-tabs-mode 1)))
