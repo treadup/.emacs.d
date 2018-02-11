6cd1ba0b8cb12fc0542211a7f4498e744e8f3baf
Henrik Treadup <henrik.treadup@gmail.com>
2018-02-11 12:48:26 +0100

    Information about conda.el

---
+++ elisp/for-conda.el
@@ -0,0 +1,9 @@
+;;;
+;;; Customizations for conda.el
+;;;
+
+;; You can read more about conda.el at the following location.
+;; https://github.com/necaris/conda.el
+
+;; Actually I'm not sure how this will interact with the normal Python
+;; virtual environments. For now I am not going to turn this on.
