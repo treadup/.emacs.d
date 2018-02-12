d7a9e3523018eb218ad27015a77f489d121096a5
Henrik Treadup <henrik.treadup@gmail.com>
2018-02-12 19:59:40 +0100

    Some functions for creating shells and terminals.

--- elisp/for-shell.el
+++ elisp/for-shell.el
@@ -22,4 +22,45 @@
 ; (global-set-key (kbd "C-c e") (lambda () (interactive) (eshell-below)))
 ; (global-set-key (kbd "C-c i") (lambda () (interactive) (ielm-below)))
 
+(defun create-shell ()
+  "creates a shell with a given name"
+  (interactive);; "Prompt\n shell name:")
+  (let ((shell-name (read-string "shell name: " nil)))
+    (shell (concat "*" shell-name "*"))))
 
+;; This is quite an interesting page
+;; https://www.emacswiki.org/emacs/EshellMultipleEshellBuffers
+;; https://github.com/DamienCassou/shell-switcher
+
+(defun create-eshell ()
+  "creates an eshell with a given name"
+  (interactive);; "Prompt\n shell name:")
+  (let ((eshell-name (read-string "eshell name: " nil)))
+    (eshell (concat "*" eshell-name "*"))))
+
+;; http://emacsredux.com/blog/2013/03/29/terminal-at-your-fingertips/
+(defun visit-term-buffer ()
+  "Create or visit a terminal buffer."
+  (interactive)
+  (if (not (get-buffer "*ansi-term*"))
+    (progn
+      (split-window-sensibly (selected-window))
+      (other-window 1)
+      (ansi-term (getenv "SHELL")))
+    (switch-to-buffer-other-window "*ansi-term*")))
+
+(defun create-term ()
+  "creates a term with a given name"
+  (interactive);; "Prompt\n shell name:")
+  (let ((term-name (read-string "term name: " nil)))
+    (term (concat "*" term-name "*"))))
+
+(defun oak-shell ()
+  (interactive)
+  (let ((default-directory "/ssh:henrik@oak:"))
+    (create-shell)))
+
+(defun oak-eshell ()
+  (interactive)
+  (let ((default-directory "/ssh:henrik@oak:"))
+    (create-eshell)))
