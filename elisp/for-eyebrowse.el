d5fa01268ea217e9c16593e81b2d8d04caee87d5
Henrik Treadup <henrik.treadup@gmail.com>
2018-02-12 20:05:18 +0100

    Eyebrowse

---
+++ elisp/for-eyebrowse.el
@@ -0,0 +1,12 @@
+;;;
+;;; Customizations for eyebrowse
+;;;
+
+;; Documentation on the eyebrowse package can be found
+;; at the following location.
+;; https://github.com/wasamasa/eyebrowse
+
+(use-package eyebrowse
+  :ensure t
+  :config
+  (eyebrowse-mode t))
