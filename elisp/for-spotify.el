aaff0a83cbb46e178e1b7ee8216f876985b4db31
Henrik Treadup <henrik.treadup@gmail.com>
2018-02-15 19:50:04 +0100

    Keybindings for spotify mode.

--- elisp/for-spotify.el
+++ elisp/for-spotify.el
@@ -3,4 +3,14 @@
 ;;;
 
 (use-package spotify
-    :ensure t)
+  :ensure t
+  :config
+  (global-set-key (kbd "C-c m s") 'spotify-playpause)
+  (global-set-key (kbd "C-c m <") 'spotify-previous)
+  (global-set-key (kbd "C-c m >") 'spotify-next))
+
+;; Emacs has a built in music player called MPC. The above keybindings
+;; are inspired by the MPC keybindings.
+
+(provide 'for-spotify)
+;;; for-spotify.el ends here
