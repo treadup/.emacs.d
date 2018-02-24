40f27bde8de557b3553ba9e5bb9f821ece454140
Henrik Treadup <henrik.treadup@gmail.com>
2018-02-24 23:15:39 +0100

    Info on Django packages for Emacs.

---
+++ elisp/for-django.el
@@ -0,0 +1,14 @@
+;;;
+;;; Customizations for Django
+;;;
+
+;; There seems to be three packages that are useful for Django development.
+
+;; pony-mode
+;; https://github.com/davidmiller/pony-mode
+
+;; django-mode
+;; https://github.com/myfreeweb/django-mode
+
+;; python-django
+;; https://github.com/fgallina/python-django.el
