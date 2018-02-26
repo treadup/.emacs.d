1ea2af83cca82a1bd5a6f7b1979a5959f5cd6941
Henrik Treadup <henrik.treadup@gmail.com>
2018-02-26 20:35:06 +0100

    Add the projectile project root to the python-shell-extra-pythonpaths. This makes the Emacs python instance aware of the code in our project.

--- elisp/for-python.el
+++ elisp/for-python.el
@@ -2,20 +2,16 @@
 ;;; Commentary:
 ;;;
 
-;; TODO:
-;; 1. Virtual environments in Emacs
-;;    Want them to work in the python shell and eshell.
-;;    Use named virtual environments and make sure that it is
-;;    compatible with pew.
-;; 2. Auto completion using company
-;; 3. Anaconda eldoc mode. I do not believe that
-;;    this is currently working. Flymake or something else
-;;    seems to be indicating that there is an error with
-;;    anaconda-eldoc-mode. However now it looks like it
-;;    might be working again.
-;; 3. Inferior shell using ipython
-;; 4. Fix the mode line so that it contains the name of the
-;;    virtual environment.
+;; This configuration does the following.
+;; 1. Activates anaconda-mode
+;; 2. Set the python-shell-extra-pythonpaths to contain the root
+;;    folder of the current projectile project.
+;; 3. TODO: Activate the virtual environment in the .venv file in the current
+;;    projectile project.
+;; 3. Turn on auto-completion using company-anaconda.
+;; 4. Turn on eldoc using anaconda-eldoc-mode
+;; 5. Use iPython for the inferior shell
+;; 6. TODO: The mode line should contain the name of the virtual environment.
 
 ;;; Code:
 (use-package anaconda-mode
@@ -26,7 +22,6 @@
     (add-hook 'python-mode-hook 'anaconda-mode)
 
     ;; Anaconda has an eldoc mode. See if I can get this working.
-    ;; TODO: I think that 'anaconda-eldoc-mode is a free variable here.
     (add-hook 'python-mode-hook 'anaconda-eldoc-mode)))
 
 (use-package company-anaconda
@@ -36,9 +31,6 @@
   ;; Add company-anaconda as a company backend.
   (eval-after-load "company"
     '(add-to-list 'company-backends 'company-anaconda)))
-
-;; I might want to think about using pyenv mode.
-;; https://github.com/proofit404/pyenv-mode
 
 ;; References
 ;; https://github.com/proofit404/anaconda-mode
@@ -57,3 +49,50 @@
 ;; the global environment.
 ;;
 ;;    pip install jedi==0.8.1 json-rpc==1.8.1 service_factory==0.1.2
+
+
+;; Use the pyvenv package to mange virtual environments.
+;; https://github.com/jorgenschaefer/pyvenv
+;; This is the package that spacemacs uses to manage virtual environments.
+(use-package pyvenv
+  :ensure t)
+
+;;
+;; Projectile switch project hook
+;;
+
+;; There are two things that you need to configure for Python in Emacs.
+;; The first is adding the root folder of the project as an extra python path.
+;; The second is activating the virtual environment.
+
+(defconst pht--original-python-extra-pythonpaths python-shell-extra-pythonpaths)
+
+(if (projectile-project-p)
+  (message (concat "Inside project" " " (projectile-project-root)))
+  (message "Not in a project."))
+
+(defun pht-activate-python-project ()
+"Activate the current projectile project as a python project.
+Sets the python-extra-pythonpath to the root of the project.
+TODO: Activates the virtual environment."
+  (if (projectile-project-p)
+    (progn
+      (setq python-shell-extra-pythonpaths pht--original-python-extra-pythonpaths)
+      (add-to-list 'python-shell-extra-pythonpaths (projectile-project-root)))))
+
+;; On startup activate the current projectile project, if there is
+;; one, as a python project.
+(pht-activate-python-project)
+
+;; And switch python project when we switch projects in Projectile.
+(add-hook 'projectile-after-switch-project-hook #'pht-activate-python-project)
+
+;;
+;; iPython
+;;
+
+;; Use ipython for the inferior shell. The --simple-prompt argument is there
+;; because Emacs does not work with the default prompt in newer versions of
+;; ipython.
+(setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "--simple-prompt -i")
