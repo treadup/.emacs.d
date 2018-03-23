9ed1e0b8520543fc266a967e93ce7c876fd061d0
Henrik Treadup <henrik.treadup@gmail.com>
2018-03-23 17:12:41 +0100

    Use the Django engine for web mode.

--- elisp/for-web-mode.el
+++ elisp/for-web-mode.el
@@ -1,14 +1,12 @@
 ;;;
 ;;; Customizations for web mode
-;;; 
+;;;
 
 ;; The web mode website is located at the following url.
 ;; http://web-mode.org/
 
-(defun personal-configure-web-mode () 
-  (progn
-    (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))))
-
 (use-package web-mode
   :ensure t
-  :config (personal-configure-web-mode))
+  :config
+  (setq web-mode-engines-alist '(("django"    . "\\.html\\'")))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
