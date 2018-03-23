c422b27fc2d2db7e59695639509973d74882d088
Henrik Treadup <henrik.treadup@gmail.com>
2018-03-23 17:10:15 +0100

    Enable goto-address-mode

--- elisp/for-emacs.el
+++ elisp/for-emacs.el
@@ -1,6 +1,11 @@
 ;;;
 ;;; Configuration of builtin emacs packages
 ;;;
-(use-package emacs
-  :diminish
-  (auto-revert-mode))
+
+;; Automatically revert a buffer if the file was changed by an external
+;; program.
+;; (global-auto-revert-mode 1)
+
+;; Make hyper links clickable in text mode and prog mode.
+(add-hook 'text-mode-hook 'goto-address-mode)
+(add-hook 'prog-mode-hook 'goto-address-mode)
