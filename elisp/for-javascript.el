a9ce8ac6532d95d139549b7da169ab47f096db81
Henrik Treadup <henrik.treadup@gmail.com>
2018-03-23 17:11:52 +0100

    Start jsx mode for .js and .jsx files.

--- elisp/for-javascript.el
+++ elisp/for-javascript.el
@@ -5,15 +5,17 @@
 ;; https://github.com/mooz/js2-mode
 
 (use-package js2-mode
-  :ensure t)
+  :ensure t
+  :config
+
+  ;; Use the jsx mode for .js and React .jsx files.
+  (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
+  (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode)))
 
 ;; Support for .js files.
 ;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 ;; (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
-;; Use the jsx mode for .js and React .jsx files.
-(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
 
 ;; Use company-tern for completion in JavaScript modes.
 (use-package company-tern
