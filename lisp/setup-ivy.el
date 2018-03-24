4b441dbdc5fb37c3e2d362f3f6e30ad251ac4343
Henrik Treadup <henrik.treadup@gmail.com>
2018-03-24 12:26:41 +0100

    Use Ivy.

--- lisp/setup-ivy.el
+++ lisp/setup-ivy.el
@@ -45,38 +45,38 @@
 ;; C-n goes to the next match.
 ;; C-p goes to the previous match.
 ;; Up and down arrow will move around in the minibuffer.
-(global-set-key "\C-s" 'swiper)
+;; (global-set-key "\C-s" 'swiper)
 
 ;; Resume the last ivy command?
 ;; Can be used to do a resume search.
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
+;;(global-set-key (kbd "C-c C-r") 'ivy-resume)
+;;(global-set-key (kbd "<f6>") 'ivy-resume)
 
-(global-set-key (kbd "M-x") 'counsel-M-x)
+;; (global-set-key (kbd "M-x") 'counsel-M-x)
 
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 
 ;;
 ;; Different help commands
 ;;
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-load-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+;; (global-set-key (kbd "<f1> l") 'counsel-load-library)
+;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
 
 ;;
 ;; Different counsel searching commands
 ;;
 
 ;; Find file in the current git repo.
-(global-set-key (kbd "C-c g") 'counsel-git)
+;; (global-set-key (kbd "C-c g") 'counsel-git)
 
 ;; Grep the current git repo
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
+;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
 
 ;; Use ag to search. (Does this stay within the repo?)
-(global-set-key (kbd "C-c k") 'counsel-ag)
+;; (global-set-key (kbd "C-c k") 'counsel-ag)
 
 ;; Use locate to find a file
-(global-set-key (kbd "C-x l") 'counsel-locate)
+;; (global-set-key (kbd "C-x l") 'counsel-locate)
