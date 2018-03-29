b0d027818f28304b8a8ad36764e2944b84039cc2
Henrik Treadup <henrik.treadup@gmail.com>
2018-03-29 17:49:16 +0200

    Setup Docker

--- lisp/setup-docker.el
+++ lisp/setup-docker.el
@@ -2,5 +2,14 @@
 ;;; Customization for Docker
 ;;;
 
+;; Docker major mode for editing Dockerfiles
 (use-package dockerfile-mode
+  :ensure t)
+
+;; Tramp integration for Docker
+(use-package docker-tramp
+  :ensure t)
+
+;; Manage Docker form Emacs
+(use-package docker
   :ensure t)
