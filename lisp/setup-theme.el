148b6120b1307779d3c1a3812dd5ef602cb18c22
Henrik Treadup <henrik.treadup@gmail.com>
2018-03-30 16:22:41 +0200

    Use package.el to install spacemacs theme.

--- lisp/setup-theme.el
+++ lisp/setup-theme.el
@@ -2,17 +2,14 @@
 ;;; Spacemacs theme
 ;;;
 
-;; Use the theme from Spacemacs
-;; (use-package spacemacs-theme
-;;  :defer t
-;;  :config
-;;   (require 'spacemacs-common)
-;;  (load-theme 'spacemacs-dark t))
-
 ;; There is an issue with the spacemacs theme not being installable using use-package.
-;; Fixed by including a hacked version of the theme file in the elisp folder.
-;; https://github.com/treadup/spacemacs-theme
-(load "spacemacs-theme.el")
+;; This has to to with the naming of the package and the names of the files in the package.
+;; Instead we can use plain old package.el to install the package.
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+(load-theme 'spacemacs-dark t)
 
 ;; Use the modeline from spacemacs
 (use-package spaceline-config
