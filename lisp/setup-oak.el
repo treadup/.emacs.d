e86f1e85f19465fabfc906ef0e4fb9f4ae846332
Henrik Treadup <henrik.treadup@gmail.com>
2018-04-05 18:53:15 +0200

    Small function to open eshell in the notes folder.

--- lisp/setup-oak.el
+++ lisp/setup-oak.el
@@ -6,3 +6,13 @@
   "Edit bookmarks on oak"
   (interactive)
   (find-file "/oak:~/code/python/redwood/data/bookmarks.json"))
+
+(defun notes-eshell ()
+"Open the notes folder in eshell locally on redwood and on oak
+otherwise"
+  (interactive)
+  (let ((default-directory
+          (unless (is-host-p "hickory")
+            "~/notes"
+            "/oak:~/notes")))
+    (eshell)))
