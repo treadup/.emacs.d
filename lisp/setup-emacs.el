4882e475de08a001425298b9d436d5bff0d3fcee
Henrik Treadup <henrik.treadup@gmail.com>
2018-04-10 18:02:37 +0200

    Enable disabled function erase-buffer.

--- lisp/setup-emacs.el
+++ lisp/setup-emacs.el
@@ -1,7 +1,8 @@
-;;;
-;;; Configuration of builtin emacs packages
-;;;
+;;; setup-emacs --- Configuration of builtin emacs packages.
 
+;;; Commentary:
+
+;;; Code:
 ;; Automatically revert a buffer if the file was changed by an external
 ;; program.
 ;; (global-auto-revert-mode 1)
@@ -9,3 +10,12 @@
 ;; Make hyper links clickable in text mode and prog mode.
 (add-hook 'text-mode-hook 'goto-address-mode)
 (add-hook 'prog-mode-hook 'goto-address-mode)
+
+;; Some functions in Emacs are disabled by default.
+;; The following will enable individual functions.
+
+;; Enable the erase-buffer function.
+(put 'erase-buffer 'disabled nil)
+
+(provide 'setup-emacs)
+;;; setup-emacs.el ends here
