adfc2c92717ab493ff2aac18d8490ea6f5f704d7
Henrik Treadup <henrik.treadup@gmail.com>
2018-04-11 17:37:18 +0200

    Create emacs-todo command.

--- lisp/setup-misc.el
+++ lisp/setup-misc.el
@@ -1,6 +1,6 @@
-;;;
-;;; Miscellaneous customizations
-;;;
+;;; setup-misc --- Miscellaneous customizations
+
+;;; Commentary:
 
 ;; This is a dumping ground for miscellaneous functions and comments.
 ;; These things usually graduate from here and are moved elsewhere.
@@ -8,5 +8,14 @@
 ;; Print current time in minibuffer.
 ;; Would be good if I had a keybinding for the above.
 
-
 ;; (current-time-string)
+
+;;; Code:
+
+(defun emacs-todo ()
+"Open Emacs todo org file."
+  (interactive)
+  (find-file "~/.emacs.d/todo.org"))
+
+(provide 'setup-misc)
+;;; setup-misc.el ends here
