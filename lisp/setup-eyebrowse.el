6f5fd47382a869bafdf1f4bb7cd88d59aebb3968
Henrik Treadup <henrik.treadup@gmail.com>
2018-04-13 17:55:52 +0200

    Fixed formatting of setup-eyebrowse.el

--- lisp/setup-eyebrowse.el
+++ lisp/setup-eyebrowse.el
@@ -1,12 +1,17 @@
-;;;
-;;; Customizations for eyebrowse
-;;;
+;;; setup-eyebrowse --- Customizations for eyebrowse
+
+;;; Commentary:
 
 ;; Documentation on the eyebrowse package can be found
 ;; at the following location.
 ;; https://github.com/wasamasa/eyebrowse
 
+;;; Code:
+
 (use-package eyebrowse
   :ensure t
   :config
   (eyebrowse-mode t))
+
+(provide 'setup-eyebrowse)
+;;; setup-eyebrowse.el ends here
