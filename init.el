0ccace38a5ef08a0ac1bb20f57436744f82457b6
Henrik Treadup <henrik.treadup@gmail.com>
2018-04-13 18:04:21 +0200

    Changed how we set customizable variables in init file.

--- init.el
+++ init.el
@@ -103,9 +103,8 @@
 ;; https://snarfed.org/gnu_emacs_backup_files
 
 ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
-(custom-set-variables
-  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
-  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+(customize-set-variable 'auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+(customize-set-variable 'backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
 
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
