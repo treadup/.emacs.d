d408db93d1f25e5e53e4e6b8f9f0503cfb1414e0
Henrik Treadup <henrik.treadup@gmail.com>
2018-04-16 20:15:43 +0200

    Formatting setup-asciidoc

--- lisp/setup-asciidoc.el
+++ lisp/setup-asciidoc.el
@@ -1,7 +1,11 @@
-;;;
-;;; Customization for ascii doc.
-;;;
+;;; setup-asciidoc --- Customization for ascii doc.
+
+;;; Commentary:
+
+;;; Code:
 
 (use-package adoc-mode
   :ensure t)
 
+(provide 'setup-asciidoc)
+;;; setup-asciidoc.el ends here
