cc9a402f86bcea9b36f01e9417f66370f38f27f5
Henrik Treadup <henrik.treadup@gmail.com>
2018-04-21 18:51:39 +0200

    Better config for projectile.

--- lisp/setup-projectile.el
+++ lisp/setup-projectile.el
@@ -1,14 +1,26 @@
-;;;
-;;; Customizations for projectile
-;;;
+;;; setup-projectile --- Customizations for projectile
+
+;;; Commentary:
+
+;;; Code:
 
 ;; Projectile is a project interaction library for Emacs.
 ;; It provides functions for project navigation.
 ;; https://www.emacswiki.org/emacs/Projectile
 (use-package projectile
   :ensure t
-  :config (projectile-global-mode) ;; Enable projectile everywhere.
-  :diminish "P")
+  :config
 
-;; (use-package projectile
-;;  :ensure t)
+  ;; Should create a hydra to use as the projectile-switch-project-action.
+  ;; For now I will use projectile commander.
+  (setq projectile-switch-project-action #'projectile-commander)
+
+  ;; Set the modeline indicator to Prj[<project name>]
+  (customize-set-variable 'projectile-mode-line
+    '(:eval (format "Prj[%s]" (projectile-project-name))))
+
+  ;; Enable projectile everywhere
+  (projectile-mode))
+
+(provide 'setup-projectile)
+;;; setup-projectile ends here
