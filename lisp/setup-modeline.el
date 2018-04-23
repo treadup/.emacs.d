99cbb7cf89a27e7823559f42535aa848fa29d415
Henrik Treadup <henrik.treadup@gmail.com>
2018-04-23 18:04:42 +0200

    Decided to keep using spaceline for the modeline.

--- lisp/setup-modeline.el
+++ lisp/setup-modeline.el
@@ -2,6 +2,8 @@
 ;;; Customizations for modeline
 ;;;
 
-;; Currently I am using spaceline for the modeline. This is configured in the for-theme.el file.
+;; Currently I am using spaceline for the modeline. This is configured in the setup-theme.el file.
 ;; Right now it does everything I want the modeline to do. For example it displays the current
 ;; python virutal environment name in the modeline.
+
+;; Do not write a custom mode line. It seems insanely time consuming to get anything good working.
