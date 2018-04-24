fadf8e48c9802f724adb20814e68265b0389d657
Henrik Treadup <henrik.treadup@gmail.com>
2018-04-24 18:30:00 +0200

    Notes on certain editing keybindings.

--- lisp/setup-editing.el
+++ lisp/setup-editing.el
@@ -18,10 +18,14 @@
   (progn
     (global-set-key (kbd "C-a") 'crux-move-beginning-of-line)
     (global-set-key (kbd "C-k") 'crux-smart-kill-line)
+    ;; You almost never need to use kill-whole-line after switching
+    ;; to using the whole-line-or-region package. Use C-w instead.
     (global-set-key (kbd "C-S-k") 'crux-kill-whole-line)
     (global-set-key (kbd "C-c 4 t") 'crux-transpose-windows)
     (global-set-key (kbd "C-c e") 'crux-eval-and-replace)
     (global-set-key (kbd "C-o") 'crux-smart-open-line)
+    ;; The smart open line above function is almost never used.
+    ;; Keep the keybinding as it is for now.
     (global-set-key (kbd "C-S-o") 'crux-smart-open-line-above)
     ))
 
