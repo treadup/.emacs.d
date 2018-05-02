0d686d361e001e6dd32fdbdb92f4b8f808b29de9
Henrik Treadup <henrik.treadup@gmail.com>
2018-05-02 18:39:38 +0200

    Changed formatting of setup-hydra.

--- lisp/setup-hydra.el
+++ lisp/setup-hydra.el
@@ -1,9 +1,13 @@
-;;;
-;;; Customizations for Hydra
-;;;
+;;; setup-hydra --- Customizations for Hydra
 
+;;; Commentary:
 
 ;; https://github.com/abo-abo/hydra/
 
+;;; Code:
+
 (use-package hydra
   :ensure t)
+
+(provide 'setup-hydra)
+;;; setup-hydra ends here
