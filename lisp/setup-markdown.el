94428ff7e3ebbcf39ebe40f1cb11ca766f1b724e
Henrik Treadup <henrik.treadup@gmail.com>
2018-05-10 18:36:51 +0200

    Use markdown instead of multimarkdown in markdown-mode.

--- lisp/setup-markdown.el
+++ lisp/setup-markdown.el
@@ -1,6 +1,6 @@
-;;;
-;;; Customization for markdown
-;;;
+;;; setup-markdown --- Customization for markdown
+
+;;; Commentary:
 
 ;; The markdown-mode package provides two major-modes
 ;; 1. markdown-mode
@@ -9,11 +9,14 @@
 ;; markdown-mode is a major mode for standard markdown.
 ;; gfm-mode is a major mode for GitHub flavored markdown.
 
+;;; Code:
+
 (use-package markdown-mode
   :ensure t
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . gfm-mode)
-         ("\\.markdown\\'" . gfm-mode))
-  :init (setq markdown-command "multimarkdown"))
+         ("\\.markdown\\'" . gfm-mode)))
 
+(provide 'setup-markdown)
+;;; setup-markdown ends here
