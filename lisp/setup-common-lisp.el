fa122cd5de2dacee3fee8159a7e5fdbfc5637804
Henrik Treadup <henrik.treadup@gmail.com>
2018-05-25 21:17:21 +0200

    Use SLIME installed by Quicklisp.

--- lisp/setup-common-lisp.el
+++ lisp/setup-common-lisp.el
@@ -4,12 +4,16 @@
 
 ;;; Code:
 
-(use-package slime
-  :ensure t
-  :config
-  (progn
-    (setq inferior-lisp-program "/usr/local/bin/sbcl")
-    (setq slime-contribs '(slime-fancy))))
+;; (use-package slime
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (setq inferior-lisp-program "/usr/local/bin/sbcl")
+;;     (setq slime-contribs '(slime-fancy))))
+
+(load (expand-file-name "~/.quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "sbcl")
+
 
 (provide 'setup-common-lisp)
 ;;; setup-common-lisp ends here
