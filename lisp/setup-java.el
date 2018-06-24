3da3f4258a3bbbf678c6adfa77c98e6997855eef
Henrik Treadup <henrik.treadup@gmail.com>
2018-06-24 20:59:43 +0200

    Stub for setting up Java that mentions Megahanda mode.

---
+++ lisp/setup-java.el
@@ -0,0 +1,12 @@
+;;; setup-java --- Customizations for Java
+
+;;; Commentary:
+
+;; There is a minor-mode called maghanda-mode that provides a better
+;; editing experience for Java in Emacs.
+;; https://github.com/mopemope/meghanada-emacs
+
+;;; Code:
+
+(provide 'setup-java)
+;;; setup-java ends here
