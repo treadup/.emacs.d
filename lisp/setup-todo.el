f4b197bd094173cd0ed7574eee6913d24d70e196
Henrik Treadup <henrik.treadup@gmail.com>
2018-09-25 19:28:50 +0200

    The toggle-work-todo function will toggle between the current buffer and the work-todo.org buffer.

--- lisp/setup-todo.el
+++ lisp/setup-todo.el
@@ -1,7 +1,25 @@
-;;;
-;;; Customizations for todo
-;;;
+;;; setup-todo --- Customizations for todo
+;;; Commentary:
+
+;;; Code:
 
 (defun work-todo ()
+  "Open the work-todo.org buffer.
+Open the ~/work/work-todo.org file if the buffer is not already open."
   (interactive)
-  (find-file "~/work/todo.org"))
+  (find-file "~/work/work-todo.org"))
+
+;; Define a function that toggles between the work-todo file and the
+;; current file. Kind of like how C-x 7 is handled currently.
+
+(defun toggle-work-todo ()
+  "Toggle between a buffer and the work-todo.org buffer/file."
+  (interactive)
+  (if (equal (buffer-name) "work-todo.org")
+    (switch-to-buffer (other-buffer))
+    (work-todo)))
+
+(global-set-key (kbd "C-x 8") 'toggle-work-todo)
+
+(provide 'setup-todo)
+;;; setup-todo ends here
