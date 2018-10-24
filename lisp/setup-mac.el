85847e749b6a74d72d38bf5e6c45f68817997183
Henrik Treadup <henrik.treadup@gmail.com>
2018-10-24 13:14:41 +0200

    Use the fn key on mac as the Hyper key.

--- lisp/setup-mac.el
+++ lisp/setup-mac.el
@@ -9,5 +9,12 @@
   :config
   (mac-pseudo-daemon-mode))
 
+;; Use the fn key as the hyper modifier which means that we can now
+;; keybind things using H+<key>.
+(setq ns-function-modifier 'hyper)
+
+;; Cmd is already the super modifier which means that we can keybind
+;; things using S+<key>.
+
 (provide 'setup-mac)
 ;;; setup-mac.el ends here
