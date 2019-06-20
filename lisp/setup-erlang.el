e7fec8691a5b771021bf4d23c6ee1e244753a6e2
Henrik Treadup <henrik.treadup@gmail.com>
2019-06-20 20:42:58 +0200

    Ensure that Erlang mode is available

--- lisp/setup-erlang.el
+++ lisp/setup-erlang.el
@@ -1,10 +1,15 @@
-;;;
-;;; Customizations for Erlang
-;;;
+;;; setup-erlang --- Customizations for Erlang
 
-(use-package erlang
-  :defer t)
+;;; Commentary:
 
 ;; The following packages look interesting.
 ;; https://melpa.org/#/company-erlang
 ;; https://melpa.org/#/company-distel
+
+;;; Code:
+
+(use-package erlang
+  :ensure t)
+
+(provide 'setup-erlang)
+;;; setup-erlang ends here
