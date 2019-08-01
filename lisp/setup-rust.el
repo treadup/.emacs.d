43ce519da6c47db68942e37673cefc6e29f34790
Henrik Treadup <henrik.treadup@gmail.com>
2019-08-01 12:08:06 +0200

    Use cargo minor mode when in rust-mode

--- lisp/setup-rust.el
+++ lisp/setup-rust.el
@@ -7,5 +7,10 @@
 (use-package rust-mode
   :ensure t)
 
+(use-package cargo
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook 'cargo-minor-mode))
+
 (provide 'setup-rust)
 ;;; setup-rust.el ends here
