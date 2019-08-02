5c70f2a04c469926bbb8c58f80b33d1e78868d62
Henrik Treadup <henrik.treadup@gmail.com>
2019-08-02 15:44:40 +0200

    In json-mode bind C-c / to format the buffer

--- lisp/setup-json.el
+++ lisp/setup-json.el
@@ -1,7 +1,13 @@
-;;;
-;;; Customization for json-mode
-;;;
+;;; setup-json --- Customization for json-mode
 
+;;; Commentary:
+
+;;; Code:
 (use-package json-mode
-  :ensure t)
+  :ensure t
+  :config
+  :bind
+  (("C-c /" . 'json-pretty-print-buffer)))
 
+(provide 'setup-json)
+;;; setup-json.el ends here
