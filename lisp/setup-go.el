25d86c70f9dcfa26754a0f24df93582226aab2f0
Henrik Treadup <henrik.treadup@gmail.com>
2022-12-02 09:27:37 +0100

    Use spaces when working with Go buffers
    Automatically transform tabs into spaces when loading Go files.
    Convert spaces to tabs before saving and then convert the tabs
    back to spaces once you are done saving the file.

--- lisp/setup-go.el
+++ lisp/setup-go.el
@@ -5,7 +5,20 @@
 (use-package go-mode
   :ensure t)
 
+;; Use spaces in Emacs buffers for Go code. Convert
+;; tabs to spaces when loading the file. Convert spaces
+;; to tabs when saving the file.
 (add-hook 'go-mode-hook
-          (lambda ()
-            ;; (add-hook 'before-save-hook 'gofmt-before-save)
-            (setq indent-tabs-mode 1)))
+  (lambda ()
+    (setq tab-width 4)
+    (setq standard-indent 4)
+    (setq indent-tabs-mode nil)
+    ;; When a file is loaded untabify it.
+    (untabify-buffer)
+    ;; Gofmt the buffer before saving it. This will add back the tabs.
+    (add-hook 'before-save-hook 'gofmt-before-save)
+    ;; Remove the tabs again after the buffer has been saved.
+    (add-hook 'after-save-hook 'untabify-buffer)))
+
+(provide 'setup-go)
+;;; setup-go.el ends here
